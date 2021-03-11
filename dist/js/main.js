7bb54530639d85bce6d42c3dd102eb6f6633a5d7
Zoltán Király <zoliky@gmail.com>
2021-03-11 19:01:48 +0200

    Sync

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,5 @@
 "use strict";
 
-console.log("ok");
-
 // Hamburger navigation
 const menuToggle = document.querySelector(".menu-toggle");
 const nav = menuToggle.parentElement;
@@ -10,4 +8,4 @@ const menuDropdown = document.querySelector(".menu-dropdown");
 menuToggle.addEventListener("click", event => {
   event.preventDefault();
   nav.classList.toggle("is-open");
-})
+});
