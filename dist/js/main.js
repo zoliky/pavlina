9a720c72636e7225ccda37ad54aaa654d3c1be0f
Zoltán Király <zoliky@gmail.com>
2021-03-11 18:57:14 +0200

    Sync

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+console.log("ok");
+
+// Hamburger navigation
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = menuToggle.parentElement;
+const menuDropdown = document.querySelector(".menu-dropdown");
+
+menuToggle.addEventListener("click", event => {
+  event.preventDefault();
+  nav.classList.toggle("is-open");
+})
--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 "use strict";
+
+console.log("ok");
+
+// Hamburger navigation
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = menuToggle.parentElement;
+const menuDropdown = document.querySelector(".menu-dropdown");
+
+menuToggle.addEventListener("click", event => {
+  event.preventDefault();
+  nav.classList.toggle("is-open");
+
+  const height = menuDropdown.scrollHeight;
+
+  if (nav.classList.contains("is-open")) {
+    menuDropdown.style.setProperty("height", height + "px");
+  } else {
+    menuDropdown.style.setProperty("height", "0");
+  }
+})
