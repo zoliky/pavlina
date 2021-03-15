31c283704b49365ae68afa3070db95ef59bffcc3
Zoltán Király <zoliky@gmail.com>
2021-03-15 17:08:39 +0200

    Sync

--- intersection-observer.js
+++ intersection-observer.js
@@ -2,13 +2,12 @@
 
 "use strict";
 
-const sectionOne = document.querySelector(".section-one");
-const sections = document.querySelectorAll("section");
+const faders = document.querySelectorAll(".fade-in");
 
 const options = {
   root: null, // defaults to the viewport
-  threshold: 0.25, // A zero value fires as soon as any little piece enters
-  rootMargin: "-150px"
+  threshold: 0, // A zero value fires as soon as any little piece enters
+  rootMargin: "-100px"
 };
 
 const observer = new IntersectionObserver(function(entries, observer) {
@@ -16,15 +15,17 @@ const observer = new IntersectionObserver(function(entries, observer) {
     if (!entry.isIntersecting) {
       return;
     }
+
+    entry.target.classList.add("appear");
     
-    console.log(entry.target);
-    console.log(entry.isIntersecting);
+    // console.log(entry.target);
+    // console.log(entry.isIntersecting);
 
     // Once observed, stop observing
     observer.unobserve(entry.target);
   });
 }, options);
 
-sections.forEach(section => {
-  observer.observe(section);
+faders.forEach(fader => {
+  observer.observe(fader);
 });
