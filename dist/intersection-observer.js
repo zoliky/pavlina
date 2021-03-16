c8c5f2ff50a93e150618e212b9f528c28f2cf94e
Zoltán Király <zoliky@gmail.com>
2021-03-16 17:33:09 +0200

    Sync

---
+++ dist/intersection-observer.js
@@ -0,0 +1,28 @@
+// Copyright 2021 Zoltán Király. All rights reserved.
+
+"use strict";
+
+const faders = document.querySelectorAll(".fade-in");
+
+const options = {
+  root: null, // defaults to the viewport
+  threshold: 0, // A zero value fires as soon as any little piece enters
+  rootMargin: "-100px"
+};
+
+const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    }
+
+    entry.target.classList.add("appear");
+    
+    // Once observed, stop observing
+    observer.unobserve(entry.target);
+  });
+}, options);
+
+faders.forEach(fader => {
+  observer.observe(fader);
+});
--- dist/js/main.js
+++ dist/js/main.js
@@ -8,4 +8,31 @@ const menuDropdown = document.querySelector(".menu-dropdown");
 menuToggle.addEventListener("click", event => {
   event.preventDefault();
   nav.classList.toggle("is-open");
+
+  const height = menuDropdown.scrollHeight;
+
+  if (nav.classList.contains("is-open")) {
+    menuDropdown.style.setProperty("height", height + "px");
+  } else {
+    menuDropdown.style.setProperty("height", "0");
+  }
 });
+
+// Hide and show header
+/*
+const header = document.querySelector(".header");
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", event => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    if (!nav.classList.contains("is-open")) {
+      header.style.top = "-140px";
+    }
+  } else {
+    header.style.top = "0";
+  }
+
+  lastScrollTop = scrollTop;
+});
+*/
