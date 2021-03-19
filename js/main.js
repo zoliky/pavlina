8dfecde64f06bc04cf054eeb33aee41987b6963f
Zoltán Király <zoliky@gmail.com>
2021-03-19 23:36:56 +0200

    Sync

--- js/main.js
+++ js/main.js
@@ -19,18 +19,18 @@ menuToggle.addEventListener("click", event => {
 });
 
 // Hide and show header
-const header = document.querySelector(".header");
-let lastScrollTop = 0;
+// const header = document.querySelector(".header");
+// let lastScrollTop = 0;
 
-window.addEventListener("scroll", event => {
-  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  if (scrollTop > lastScrollTop) {
-    if (!nav.classList.contains("is-open")) {
-      header.style.top = "-150px";
-    }
-  } else {
-    header.style.top = "0";
-  }
+// window.addEventListener("scroll", event => {
+//   let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//   if (scrollTop > lastScrollTop) {
+//     if (!nav.classList.contains("is-open")) {
+//       header.style.top = "-150px";
+//     }
+//   } else {
+//     header.style.top = "0";
+//   }
 
-  lastScrollTop = scrollTop;
-});
+//   lastScrollTop = scrollTop;
+// });
